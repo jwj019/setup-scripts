ddccefa66fd17049193db72d90ba02dc21165ce0
jwj019 <jwj019@gmail.com>
2019-12-24 15:46:34 -0500

    Added node setup

---
+++ setup-node.sh
@@ -0,0 +1,16 @@
+#install node
+echo "*** Installing Node ***"
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+#install yarn
+echo "*** Installing Yarn ***"
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get -y update && sudo apt-get install -y yarn 
+
+#install other npm dependencies
+echo "*** Installing NPM stuff ***"
+sudo npm install -g npm
+sudo npm install -g yo
+sudo npm install -g generator-jhipster
