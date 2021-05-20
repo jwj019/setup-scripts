63e70f7f0c2304d5a001a7944ec69d7df404df01
Justin Johnson <jwj019@gmail.com>
2021-05-20 15:35:37 -0400

    Update versions & remove docker install for WSL 2

--- setup-wsl.sh
+++ setup-wsl.sh
@@ -8,7 +8,6 @@ sudo apt install -y \
     ca-certificates \
     python3 \
     python3-pip \
-    python-pip \
     jq
 
 #Install ansible
@@ -19,29 +18,13 @@ sudo apt install ansible -y
 sudo apt install awscli -y
 
 #Install terraform
-TERRAFORM_VERSION=0.12.12
-wget -q "https://releases.hashicorp.com/terraform/0.12.12/terraform_$(echo $TERRAFORM_VERSION)_linux_amd64.zip"
+TERRAFORM_VERSION=0.15.4
+wget -q "https://releases.hashicorp.com/terraform/$(echo $TERRAFORM_VERSION)/terraform_$(echo $TERRAFORM_VERSION)_linux_amd64.zip"
 unzip "terraform_$(echo $TERRAFORM_VERSION)_linux_amd64.zip"
 sudo mv terraform /usr/local/bin/terraform
 rm "terraform_$(echo $TERRAFORM_VERSION)_linux_amd64.zip"
-
-#install docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get update -y
-sudo apt-get install -y docker-ce
-sudo usermod -aG docker $USER
-sudo chown $USER ~/.docker
 
 #Install docker-compose
 COMPOSE_VERSION=1.24.1
 sudo curl -L "https://github.com/docker/compose/releases/download/$(echo $COMPOSE_VERSION)/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-
-
-#Hook up docker to docker for windows
-echo "export DOCKER_HOST=tcp://localhost:2375" >> ~/.bashrc && source ~/.bashrc
-
